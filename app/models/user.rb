ea5169f964783f39be60f2653fbe0e48f8c94fde
gwq <1305851004@qq.com>
2018-12-09 10:51:46 +0800

    delete rails admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,4 @@ class User < ActiveRecord::Base
   has_secure_password
   enum gender: ["female", "male"]
   has_many :orders
-
-  rails_admin do
-    navigation_label '<i class="fa fa-user" aria-hidden="true">用户</i>'.html_safe
-    field :name
-    field :gender
-  end
 end
